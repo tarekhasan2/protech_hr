646e042eb40c3d86ef0c6a30cfe62ec1962751b3
tarekhasan2 <hasan867656@gmail.com>
2023-07-24 18:37:17 +0600

    update code

--- autonomous-hr-chatbot/hr_agent_backend_azure.py
+++ autonomous-hr-chatbot/hr_agent_backend_azure.py
@@ -2,17 +2,20 @@
 import pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
-from langchain.chat_models import AzureChatOpenAI
+from langchain.chat_models import AzureChatOpenAI, ChatOpenAI
 from langchain.chains import RetrievalQA
 # load agents and tools modules
 import pandas as pd
 from azure.storage.filedatalake import DataLakeServiceClient
 from io import StringIO
 from langchain.tools.python.tool import PythonAstREPLTool
-from langchain.agents import initialize_agent, Tool
-from langchain.agents import AgentType
+from langchain.agents import ZeroShotAgent, Tool, AgentExecutor, AgentType
 from langchain import LLMMathChain
 import os 
+from langchain.memory import ConversationBufferMemory
+from langchain import OpenAI, LLMChain
+
+
 
 PINECONE_API_KEY        = os.environ['PINECONE_API_KEY']
 PINECONE_API_ENV        = os.environ['PINECONE_API_ENV']
@@ -81,10 +84,6 @@ csv_file = StringIO(file)
 df = pd.read_csv(csv_file) # load employee_data.csv as dataframe
 python = PythonAstREPLTool(locals={"df": df}) # set access of python_repl tool to the dataframe
 
-csv_file = StringIO(file)
-df = pd.read_csv(csv_file) # load employee_data.csv as dataframe
-python = PythonAstREPLTool(locals={"df": df}) # set access of python_repl tool to the dataframe
-
 # create calculator tool
 calculator = LLMMathChain.from_llm(llm=llm, verbose=True)
 
@@ -95,20 +94,20 @@ df_columns = df.columns.to_list() # print column names of df
 # prep the (tk policy) vectordb retriever, the python_repl(with df access) and langchain calculator as tools for the agent
 tools = [
     Tool(
-        name = "Timekeeping Policies",
-        func=employee_policy.run,
+        name = "Employee Policies",
+        func = employee_policy.run,
         description="""
-        Useful for when you need to answer questions about employee timekeeping policies.
+        Useful for when you need to answer questions about employee policies, rules and regulations. You should give priority to using it.
 
         <user>: What is the policy on unused vacation leave?
-        <assistant>: I need to check the timekeeping policies to answer this question.
-        <assistant>: Action: Timekeeping Policies
+        <assistant>: I need to check the policies to answer this question.
+        <assistant>: Action: Employee Policies
         <assistant>: Action Input: Vacation Leave Policy - Unused Leave
         ...
         """
     ),
     Tool(
-        name = "Employee Data",
+        name = "Employee Database",
         func=python.run,
         description = f"""
         Useful for when you need to answer questions about employee data stored in pandas dataframe 'df'. 
@@ -119,6 +118,7 @@ tools = [
         <assistant>: df[df['name'] == '{user}']['sick_leave']
         <assistant>: You have n sick leaves left.              
         """
+
     ),
     Tool(
         name = "Calculator",
@@ -129,18 +129,39 @@ tools = [
     )
 ]
 
-# change the value of the prefix argument in the initialize_agent function. This will overwrite the default prompt template of the zero shot agent type
-agent_kwargs = {'prefix': f'You are friendly HR assistant. You are tasked to assist the current user: {user} on questions related to HR. You have access to the following tools:'}
 
+prefix = """You are friendly HR assistant. You are tasked to answer questions related to HR. You have access to the following tools:"""
+suffix = """Begin!"
+{chat_history}
+Question: {input}
+{agent_scratchpad}"""
 
-# initialize the LLM agent
-agent = initialize_agent(tools, 
-                         llm, 
-                         agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, 
-                         verbose=True, 
-                         agent_kwargs=agent_kwargs
-                         )
+prompt = ZeroShotAgent.create_prompt(
+    tools,
+    prefix=prefix,
+    suffix=suffix,
+    input_variables=["input", "chat_history", "agent_scratchpad"],
+)
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+agent_chain = AgentExecutor.from_agent_and_tools(
+    agent=agent, tools=tools, verbose=True, memory=memory
+)
+
 # define q and a function for frontend
 def get_response(user_input):
-    response = agent.run(user_input)
+    try:
+        response = agent_chain.run(user_input)
+    
+    except Exception as e:
+        response = str(e)
+        if response.startswith("Could not parse LLM output: `"):
+            response = response.removeprefix("Could not parse LLM output: `").removesuffix("`")
+            return response
+        else:
+            raise Exception(str(e))
+
     return response
